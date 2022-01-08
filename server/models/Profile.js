5c3fbf7d85c14de7642784456986fbc2b0c55e47
Ahmad Habib <ahmadalkali69@gmail.com>
2022-01-08 14:32:17 +0100

    adjusted profile model based on review

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,40 +1,101 @@
 import mongoose from 'mongoose'
 
 const profileSchema = mongoose.Schema({
-	profile_photo: {
+  profilePhoto: {
     type: String,
   },
-	first_name: {
-    type: String,
-    required: true,
-  },
-	last_name: {
+  firstName: {
     type: String,
     required: true,
   },
-	gender: {
+  lastName: {
     type: String,
-    enum: ['male', 'female'],
+    required: true,
   },
-	birth_date: {
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other'],
+  },
+  birthDate: {
     type: Date,
     required: true,
   },
-	phone_number: {
+  phoneNumber: {
     type: String,
   },
-	address: {
+  address: {
     type: String,
   },
-	description: {
+  description: {
     type: String,
   },
-	availability: [
+  availability: [
     {
-      startTime: Date,
-      endTime: Date,
+      day: {
+        type: String,
+        enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      },
+      date: Date,
+      startTime: {
+        type: String,
+        enum: [
+          '1 am',
+          '2 am',
+          '3 am',
+          '4 am',
+          '5 am',
+          '6 am',
+          '7 am',
+          '8 am',
+          '9 am',
+          '10 am',
+          '11 am',
+          '12 am',
+          '1 pm',
+          '2 pm',
+          '3 pm',
+          '4 pm',
+          '5 pm',
+          '6 pm',
+          '7 pm',
+          '8 pm',
+          '9 pm',
+          '10 pm',
+          '11 pm',
+          '12 pm',
+        ],
+      },
+      endTime: {
+        type: String,
+        enum: [
+          '1 am',
+          '2 am',
+          '3 am',
+          '4 am',
+          '5 am',
+          '6 am',
+          '7 am',
+          '8 am',
+          '9 am',
+          '10 am',
+          '11 am',
+          '12 am',
+          '1 pm',
+          '2 pm',
+          '3 pm',
+          '4 pm',
+          '5 pm',
+          '6 pm',
+          '7 pm',
+          '8 pm',
+          '9 pm',
+          '10 pm',
+          '11 pm',
+          '12 pm',
+        ],
+      },
     },
   ],
 })
 
-module.exports = Profile = mongoose.model("profile", profileSchema);
+module.exports = Profile = mongoose.model("Profile", profileSchema);
